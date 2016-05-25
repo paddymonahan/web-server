cbec1f34b229f69b83a9806180f6a66813af3ea3
Paddy Monahan <padmon29@gmail.com>
2016-05-25 13:33:48 +0100

    Initialize repo

---
+++ server.js
@@ -0,0 +1,31 @@
+var PORT = 3000; //capitals mean that port is a constant
+var express = require('express');
+var app = express();
+
+var middleware = {
+	requrieAuthentication: function(req,res,next){
+		console.log('private route hit');
+		next();
+	},
+	logger: function(req,res,next){
+		console.log(req.method);
+		next();
+	}
+}
+
+app.use(middleware.logger);
+
+// app.use(middleware.requrieAuthentication);
+//app.use adds application level middleware (APPWIDE)
+
+//route specific middleware add middleware as second argument of route
+app.get('/about',middleware.requrieAuthentication ,function(req,res){
+	res.send('I\'m making a server');
+});
+
+app.use(express.static(__dirname + '/public'));
+// console.log(__dirname);
+
+app.listen(PORT,function(){
+	console.log('Server is up on port '+PORT);
+});
